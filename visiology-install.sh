9baf3a9bc3e8d8b196e6fbafdd7660510b81c7e8
nmmkn <64001157+nmmkn@users.noreply.github.com>
2023-04-13 21:46:46 +0300

    Update visiology-install.sh

--- visiology-install.sh
+++ visiology-install.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
 
+echo "Выберите вверсию для установки:"
+echo "1 версия 2.30"
+echo "2 версия 2.31"
+echo "3 Выход"
+
+read choice
+
+case $choice in
+1)
+platform_version=2.30+3.1
+;;
+2)
+platform_version=2.31+3.2
+;;
+3)
+exit 0
+;;
+*)
+echo "Выберите вверсию для установки:"
+
+esac
+
+distr_filename=distrib_$platform_version.tar.xz
+distr_folder=distrib_$platform_version

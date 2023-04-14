2ea8ab80cdae3fbdb2a966343ed00280aeaa3999
nmmkn <64001157+nmmkn@users.noreply.github.com>
2023-04-14 17:16:01 +0300

    Update platform-install.sh

--- ubuntu/resourses/platform-install.sh
+++ ubuntu/resourses/platform-install.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+
 . ./config.sh
 
 cd $HOME_DIR
-wget "https://storage.yandexcloud.net/distributions/${VP_DISTR}"
-tar -xvf ${VP_DIST}
+wget "https://storage.yandexcloud.net/distributions/$VP_DISTR"
+tar -xvf $VP_DIST
 cd $WORK_DIR
 chmod +x *.sh
 
 docker load < images/platform-deployment.tar.gz
-docker run -it --rm -u $(id -u):$(id -g) -v "$(pwd)":/mnt/volume -v /var/lib/visiology/certs:/mnt/visiology cr.yandex/crpe1mi33uplrq7coc9d/visiology/release/platform-deployment:${VP_REALESE}
+docker run -it --rm -u $(id -u):$(id -g) -v "$(pwd)":/mnt/volume -v /var/lib/visiology/certs:/mnt/visiology cr.yandex/crpe1mi33uplrq7coc9d/visiology/release/platform-deployment:$VP_REALESE
 
 ./load_images.sh -v $VP_VERSION
 
